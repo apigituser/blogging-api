91369eaf4cfdb933fe7e1ea2275d2b0c8e4121fe
apigituser <responsemail850@gmail.com>
2025-06-22 23:04:49 +0530

    post creation test[SUCCESS]

--- BloggingApp/views.py
+++ BloggingApp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from .models import Post
 
-# Create your views here.
+@csrf_exempt
+def createPost(request):
+    if request.method == "POST":
+        title = "Test Title"
+        content = "Test content"
+        category = "Random"
+        tags = "Random,Test,API"
+
+        post = Post.objects.create(
+            title=title, 
+            content=content,
+            category=category, 
+            tags=tags
+        )
+        
+        post.save()
+        return JsonResponse({"data": "created successfully"})
