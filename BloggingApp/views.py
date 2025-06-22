112f860b58a992883eb0bb56b72444ac3a6b40f6
apigituser <responsemail850@gmail.com>
2025-06-23 00:15:05 +0530

    get single post

--- BloggingApp/views.py
+++ BloggingApp/views.py
@@ -13,6 +13,15 @@ def get_create_posts(request):
     else:
         return JsonResponse({"bad request": "invalid http method"}, status=400)
     
+@csrf_exempt
+def get_delete_post(request, id):
+    if request.method == "GET":
+        return getSinglePost(request, id)
+    elif request.method == "DELETE":
+        return deletePost(request, id)
+    else:
+        return JsonResponse({"bad request": "invalid http method"}, status=400)
+
 def getPosts(request):
     posts = list(Post.objects.values())
     return JsonResponse({"posts": posts})
@@ -34,25 +43,36 @@ def createPost(request):
     )
     post.save()
 
-    postDate = datetime.datetime.strftime(post.createdAt, format("%Y-%m-%dT%H:%M:%SZ"))
-
     return JsonResponse({
         "id": int(post.id),
         "title": title,
         "content": content,
         "category": category,
         "tags": tags.split(","),
-        "createdAt": postDate,
-        "updatedAt": postDate
+        "createdAt": post.createdAt,
+        "updatedAt": post.updatedAt
     }, status=201)
 
-@csrf_exempt
 def deletePost(request, id):
-    if request.method == "DELETE":
-        try:
-            post = Post.objects.get(id=id)
-            Post.delete(post)
-            return JsonResponse({"success": "blog deleted successfully"}, status=204)
-        except Exception:
-            return JsonResponse({"not found": f"post with id {id} not found"}, status=404)
-    return JsonResponse({"bad request": "invalid http method"}, status=400)
+    try:
+        post = Post.objects.get(id=id)
+        Post.delete(post)
+        return JsonResponse({"success": "blog deleted successfully"}, status=204)
+    except Exception:
+        return JsonResponse({"not found": f"post with id {id} not found"}, status=404)
+
+def getSinglePost(request, id):
+    try:
+        post = Post.objects.get(id=id)
+
+        return JsonResponse({
+            "id": id,
+            "title": post.title,
+            "content": post.content,
+            "category": post.category,
+            "tags": post.tags.split(","),
+            "createdAt": post.createdAt,
+            "updatedAt": post.updatedAt
+        })
+    except Exception:
+        return JsonResponse({"not found": f"post with id {id} not found"}, status=404)
