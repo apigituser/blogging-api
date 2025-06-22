349479f0ad939d854760382e16a652b7131ce374
apigituser <responsemail850@gmail.com>
2025-06-22 23:33:50 +0530

    post create response

--- BloggingApp/views.py
+++ BloggingApp/views.py
@@ -1,14 +1,19 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .models import Post
+import datetime
 
 @csrf_exempt
 def createPost(request):
+
     if request.method == "POST":
-        title = "Test Title"
-        content = "Test content"
-        category = "Random"
-        tags = "Random,Test,API"
+        try:
+            title = request.POST["title"]
+            content = request.POST["content"]
+            category = request.POST["category"]
+            tags = request.POST["tags"]
+        except Exception:
+            return JsonResponse({"bad request": "provide valid parameters"}, status=400)
 
         post = Post.objects.create(
             title=title, 
@@ -16,6 +21,17 @@ def createPost(request):
             category=category, 
             tags=tags
         )
-        
         post.save()
-        return JsonResponse({"data": "created successfully"})
+
+        postDate = datetime.datetime.strftime(post.createdAt, format("%Y-%m-%dT%H:%M:%SZ"))
+
+        return JsonResponse({
+            "id": int(post.id),
+            "title": title,
+            "content": content,
+            "category": category,
+            "tags": tags.split(","),
+            "createdAt": postDate,
+            "updatedAt": postDate
+        }, status=201)
+    return JsonResponse({"bad request": "invalid http method"}, status=400)
