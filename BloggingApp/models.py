298092bfdbb213f64e4e6ef40d1072eb2ca90573
apigituser <responsemail850@gmail.com>
2025-06-22 22:48:35 +0530

    model creation

--- BloggingApp/models.py
+++ BloggingApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=200)
+    category = models.CharField(max_length=20)
+    tags = models.CharField(max_length=30)
+    createdAt = models.DateTimeField(auto_now_add=True) ## Set when object is created
+    updatedAt = models.DateTimeField(auto_now=True)     ## Set every time object is updated
